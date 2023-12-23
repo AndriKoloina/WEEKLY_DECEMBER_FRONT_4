d96ae8dc00cc6c31dd62bf65883414db0146aec6
AndriKoloina <raherimananakoloina@gmail.com>
2023-12-23 16:06:51 +0100

    1 er commit

---
+++ js/index.js
@@ -0,0 +1,139 @@
+function _$(elts) { return document.getElementById(elts) }
+function _createElts(elts) { return document.createElement(elts)}
+
+const nav_bar = _$("nav-bar")
+const btn_1 = _$("btn1")
+const btn_2 = _$("btn2")
+const btn_3 = _$("btn3")
+const info_container = _$("info-container")
+const box_container_one = _$("box-container-one")
+const box_container_two = _$("box-container-two")
+const img_content1 = _$("img-content1")
+const img_content2 = _$("img-content2")
+
+let info_image = ["img/Vector (9).png","img/Vector (8).png","img/Vector (7).png"]
+let info_txt = ["A single source of truth","Intuitive interface","Or with rules"]
+let tab_nav_bar = ["Home","Product","About","Contact"]
+
+
+let data_txt = "When you add work to your Slate calendar we automatically calculate useful insights"
+let content_txt = "Ever wondered if you're too reliant on a client for work? Slate helps you identify ."
+const navLink = (selector, table) => {
+    table.forEach((data) => {   
+  let lien = _createElts("td")
+      selector.appendChild(lien)
+  
+      let lien_href = _createElts("span")
+      lien.appendChild(lien_href)
+  
+      let a = _createElts("a")
+      a.href = "#"
+      a.textContent = data
+      a.classList.add("lien_href")
+      lien_href.appendChild(a)
+    })
+  }
+
+  navLink(nav_bar,tab_nav_bar)
+
+  const buttonCheck = (selector,height,width,value,backgroundColor, color,borderColor,radius,marginTop,marginLeft) => {
+    let t_btn = _createElts("button")
+    selector.appendChild(t_btn)
+
+    t_btn.style.height = height + "rem"
+    t_btn.style.width = width + "rem"
+    t_btn.textContent = value
+    t_btn.style.background = backgroundColor
+    t_btn.style.color = color
+    t_btn.style.borderColor = borderColor
+    t_btn.style.borderRadius = radius + "px"
+    t_btn.classList.add("btn_1")
+    t_btn.style.marginTop = marginTop + "rem"
+    t_btn.style.marginLeft = marginLeft + "rem"
+    t_btn.classList.add("style-element")
+    t_btn.classList.add("button-hover")
+}
+
+buttonCheck(btn_1,2.8,8,"Login","transparent","white","white",0,0.4,12)
+buttonCheck(btn_2,3,9,"Try For Free","rgba(68, 82, 254, 1)","white","transparent",0,0,0)
+buttonCheck(btn_3,3,9,"Learn More","transparent","white","white",0,0,"")
+
+const image_grid = (selector,data,width,height,marginTop,marginLeft) =>{
+    selector.src = data
+    selector.style.width = width + "rem"
+    selector.style.height = height + "rem"
+    selector.style.marginTop = marginTop + "rem"
+    selector.style.marginLeft = marginLeft + "rem"
+}
+
+  const icon_grid = (selector,table) =>{
+      table.forEach((data) => {
+      let t_div = _createElts("div")
+      let t_img = _createElts("img")
+      selector.appendChild(t_div)
+      t_div.appendChild(t_img)
+      image_grid(t_img,data,"1","1","-0.5")
+      })
+  }
+
+const loop_info = (selector,table1,table2,data) =>{
+    for (let i = 0; i < 3 ; i++){
+        let t_div = _createElts("div")
+        selector.appendChild(t_div)
+        t_div.style.marginTop = "5%"
+
+        let t_div1 = _createElts("div")
+        t_div.appendChild(t_div1)
+        t_div1.classList.add("inline")
+
+        let t_div1_1 = _createElts("img")
+        t_div1.appendChild(t_div1_1)
+
+        let t_div1_2 = _createElts("h2")
+        t_div1.appendChild(t_div1_2)
+
+        let t_div2 = _createElts("div")
+        t_div.appendChild(t_div2)
+
+        image_grid(t_div1_1,table1[i],1.5,1.5,0,0)
+
+        t_div1_2.textContent = table2[i]
+        t_div1_2.classList.add("text")
+        t_div2.textContent = data
+        t_div2.classList.add("text_p")
+    }
+}
+loop_info(info_container,info_image,info_txt,data_txt)
+
+const graph =(selector,height,width,color,radius) => {
+    selector.style.width = width + "rem"
+    selector.style.height = height + "rem"
+    selector.style.background = color
+    selector.style.borderRadius = radius + "rem"
+}
+
+const box_loop_container = (selector,height,width,value,background,color,borderColor,radius,marginTop,marginLeft,src,width1,height1,marginTop1,marginLeft1,data1,data2) => {
+        graph(selector,33,23,"white",1)    
+        selector.classList.add("space")
+        let t_div1 = _createElts("div")
+        selector.appendChild(t_div1)
+        t_div1.classList.add("info-title-color")
+
+        let t_div2 = _createElts("div")
+        selector.appendChild(t_div2)
+        t_div2.classList.add("info-txt-element")
+
+        let t_div3 = _createElts("div")
+        selector.appendChild(t_div3)
+
+        let t_div4 = _createElts("img")
+        selector.appendChild(t_div4)
+
+        t_div1.textContent = data1
+        t_div2.textContent = data2
+        buttonCheck(t_div3,height,width,value,background,color,borderColor,radius,marginTop,marginLeft)
+        image_grid(t_div4,src,width1,height1,marginTop1,marginLeft1)
+}
+
+box_loop_container(box_container_one,2.8,8,"Sign up","rgba(68, 82, 254, 1)","white","transparent",0,4,6.8,"img/Macbook Pro.png",18,"auto",3,2,"Work",content_txt)
+box_loop_container(box_container_two,2.8,10,"Try For Free","rgba(68, 82, 254, 1)","white","transparent",0,2,6.8,"img/Boards Notifications.png",20,"auto",1,2,"Design with real data",content_txt)
